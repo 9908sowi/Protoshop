f80fe4f040da197112bfab23315338d4fd8f6cb4
9908sowi <william.soder@edu.jonkoping.se>
2018-03-01 21:21:12 +0100

    Added controllers and views for products and categories

---
+++ Protoshop/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Protoshop.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i namn!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i en beskrivning!")]
+        [StringLength(500, MinimumLength = 10)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Du måste ange ett pris!")]
+        [Range(0,10000,ErrorMessage = "Priset måste vara mellan 0kr och 10000kr!")]
+        public double Price { get; set; }
+        public string ImageFile { get; set; }
+        public int? CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
